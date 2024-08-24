a54e4bc2428a377a65747defc03a970b1160c8f3
Jeevankumar-s <jeevenkumar2003@gmail.com>
2024-08-24 18:31:53 +0530

    added navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaCar, FaSearch, FaMapMarkerAlt, FaGlobe } from "react-icons/fa";
 import "./index.css";
 
 export default function Navbar() {
@@ -9,12 +10,7 @@ export default function Navbar() {
           <div className="brand">
             <div className="brand-logo">
               <a href="https://www.carwale.com/" title="CarWale">
-                <img
-                  width={136}
-                  alt="CarWale"
-                  src="https://imgd.aeplcdn.com/0x0/cw/static/icons/new-header/logo.svg"
-                  title="CarWale"
-                />
+                <FaCar size={50} color="blue" />
               </a>
             </div>
           </div>
@@ -44,17 +40,7 @@ export default function Navbar() {
                         className="search-input"
                       />
                       <div className="search-icon">
-                        <svg
-                          aria-hidden="true"
-                          fill="currentcolor"
-                          focusable="false"
-                          role="img"
-                          tabIndex="-1"
-                          viewBox="0 0 16 16"
-                          style={{ width: "20px", height: "20px" }}
-                        >
-                          <path d="M6.55 13.05a6.5 6.5 0 116.5-6.5 6.42 6.42 0 01-6.5 6.5zm0-12a5.5 5.5 0 000 11 5.43 5.43 0 005.5-5.5 5.51 5.51 0 00-5.5-5.5zm9.13 14.64a.54.54 0 000-.78l-4.14-4.14s-.19.22-.41.43-.37.35-.37.35l4.14 4.14a.56.56 0 00.78 0z" />
-                        </svg>
+                        <FaSearch size={20} />
                       </div>
                     </div>
                   </div>
@@ -64,31 +50,11 @@ export default function Navbar() {
             <div className="location" title="Location">
               <span className="location-badge" />
               <div>
-                <svg
-                  aria-hidden="true"
-                  fill="currentcolor"
-                  focusable="false"
-                  role="img"
-                  tabIndex="-1"
-                  viewBox="0 0 16 16"
-                  style={{ width: "24px", height: "24px" }}
-                >
-                  <path d="M8 16a1 1 0 01-.74-.33c-1-1.06-4-4.92-4.86-8.56-.5-2.64.31-4.86 2.22-6.11a6.08 6.08 0 017.76 0c1.92 1.25 2.72 3.47 2.22 6.11-.87 3.64-3.88 7.5-4.86 8.56A1 1 0 018 16zm0-15a5.08 5.08 0 00-3.16 1.07C3.13 2.7 2.41 4.47 2.87 6.8c.83 3.42 3.65 7.1 4.76 8.27 1.11-1.17 3.93-4.85 4.76-8.27.5-2.33-.26-4.1-1.97-5.73A5.08 5.08 0 008 1zM8 9a2 2 0 112-2 2 2 0 01-2 2zm0-3a1 1 0 101 1 1 1 0 00-1-1z" />
-                </svg>
+                <FaMapMarkerAlt size={24} />
               </div>
             </div>
             <div className="language-icon" title="Language">
-              <svg
-                aria-hidden="true"
-                fill="currentcolor"
-                focusable="false"
-                role="img"
-                tabIndex="-1"
-                viewBox="0 0 16 16"
-                style={{ width: "24px", height: "24px" }}
-              >
-                <path d="M6.89 12H9.11a.52.52 0 00.5-.37l.58-1.63h1.69a.5.5 0 00.49-.67l-3-8a.5.5 0 00-.94 0l-3 8a.5.5 0 00.06.47.49.49 0 00.43.2h1.69l.58 1.63a.52.52 0 00.5.37zM8 2.52l1.31 3.48H6.69L8 2.52zm-1.14 4.5h2.28l.62 1.73H6.24l.62-1.73zM15 15H1a1 1 0 000 2h14a1 1 0 000-2z" />
-              </svg>
+              <FaGlobe size={24} />
             </div>
           </div>
         </div>
