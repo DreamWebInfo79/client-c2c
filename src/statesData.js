83651b6c05764d5cdc1160e1a4192a8560939219
kiruthi <priyakiruthi21@gmail.com>
2024-08-26 00:18:35 +0530

    added state images

--- src/statesData.js
+++ src/statesData.js
@@ -1,8 +1,19 @@
-// src/statesData.js
 export const statesData = [
-    { name: 'Andhra Pradesh', image: '/path/to/andhra-pradesh.jpg' },
-    { name: 'Arunachal Pradesh', image: '/path/to/arunachal-pradesh.jpg' },
-    // Add more states as needed
-    { name: 'West Bengal', image: '/path/to/west-bengal.jpg' }
-  ];
-  
+  { name: 'Andhra Pradesh', image: '/states/Andhra_Pradesh.jpg' }, // Tirupati Temple
+  { name: 'Arunachal Pradesh', image: '/states/Arunachal_Pradesh.jpg' }, // Tawang Monastery
+  { name: 'West Bengal', image: '/states/West_Bengal.jpg' }, // Victoria Memorial
+  { name: 'Tamilnadu', image: '/states/Tamilnadu.jpg' }, // Meenakshi Temple, Madurai
+  { name: 'Karnataka', image: '/states/Kamataka.jpg' }, // Mysore Palace
+  { name: 'Kerala', image: '/states/Kerala.png' }, // Alleppey Backwaters
+  { name: 'Jharkhand', image: '/states/Jharkhand.jpg' }, // Baidyanath Temple
+  { name: 'Himachal Pradesh', image: '/states/Himachal_Pradesh.jpeg' }, // Shimla Ridge
+  { name: 'Haryana', image: '/states/Haryana.jpg' }, // Sultanpur Bird Sanctuary
+  { name: 'Gujarat', image: '/states/Gujarat.jpg' }, // Statue of Unity
+  { name: 'Delhi', image: '/states/Delhi.jpg' }, // India Gate
+  { name: 'Chhattisgarh', image: '/states/Chhattisgarh.jpg' }, // Chitrakoot Falls
+  { name: 'Bihar', image: '/states/Bihar.jpg' }, // Mahabodhi Temple
+  { name: 'Assam', image: '/states/Assam.jpg' }, // Kamakhya Temple
+  { name: 'Manipur', image: '/states/Manipur.jpg' }, // Loktak Lake
+  { name: 'Meghalaya', image: '/states/Meghalaya.jpg' }, // Living Root Bridges
+  { name: 'Puducherry', image: '/states/Puducherry.jpg' }, // Auroville
+];
