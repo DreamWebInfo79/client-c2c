e05aa52051d1bc37517f2e8588d8953c6e0ab785
Jeevankumar-s <jeevenkumar2003@gmail.com>
2024-08-25 15:53:56 +0530

    added logo and modified navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,52 +9,44 @@ export default function Navbar() {
         <div className="container">
           <div className="brand">
             <div className="brand-logo">
-              <a href="https://www.carwale.com/" title="CarWale">
-                <FaCar size={50} color="blue" />
-              </a>
+              <img alt='c2c-logo' className="logo-c2c" src='/assets/C2Clogo.jpg'/>
+              <h1 className="brand-name-c2c">C2C</h1>
             </div>
           </div>
           <nav className="nav">
             <ul>
               <li>
-                <div className="nav-item">NEW CARS</div>
+                <div className="nav-item">CARS</div>
               </li>
               <li>
-                <div className="nav-item">USED CARS</div>
-              </li>
-              <li>
-                <div className="nav-item">REVIEWS & NEWS</div>
+                <div className="nav-item">My CARS</div>
               </li>
             </ul>
           </nav>
-          <div className="search">
-            <div className="search-container" title="Search">
-              <div className="search-box">
-                <div className="search-input-container" tabIndex="-1">
-                  <div className="search-input-wrapper">
-                    <div>
-                      <input
-                        type="text"
-                        placeholder="Search"
-                        aria-label="Input field"
-                        className="search-input"
-                      />
-                      <div className="search-icon">
-                        <FaSearch size={20} />
-                      </div>
-                    </div>
+          <div className="actions">
+            <div className="search">
+              <div className="search-container" title="Search">
+                <div className="search-box">
+                  <input
+                    type="text"
+                    placeholder="Search by car name or brand"
+                    aria-label="Search by car name or brand"
+                    className="search-input"
+                  />
+                  <div className="search-icon">
+                    <FaSearch size={20} />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="location" title="Location">
-              <span className="location-badge" />
-              <div>
+            <div className="icons">
+              <div className="location" title="Location">
+                <span className="location-badge" />
                 <FaMapMarkerAlt size={24} />
               </div>
-            </div>
-            <div className="language-icon" title="Language">
-              <FaGlobe size={24} />
+              <div className="language-icon" title="Language">
+                <FaGlobe size={24} />
+              </div>
             </div>
           </div>
         </div>
