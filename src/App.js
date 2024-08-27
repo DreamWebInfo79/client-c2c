4de1b79b385657cf481c637a87c4d5a568f0277c
Jeevankumar-s <jeevenkumar2003@gmail.com>
2024-08-27 16:00:43 +0530

    added home image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <Navbar />
-    <h1>Hello World</h1>
-    <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes here as needed */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
